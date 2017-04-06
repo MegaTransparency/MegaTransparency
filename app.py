f19d3394b0503eadb99c1674a7f2e16c510ab489
Tim Clemans <timacbackup@gmaill.com>
2017-04-06 05:10:37 +0000

    fix bug

--- app.py
+++ app.py
@@ -269,9 +269,10 @@ def log_page_view():
             data['url'] = data['url'][:request.url.index('?')]
         data['time_arrived'] = calendar.timegm(time.gmtime())*1000
         data['referrer'] = request.referrer
-        if data.get('referrer', '').startswith('https://megatransparency.com/'):
-            if '?' in data.get('referrer', ''):
-                data['referrer'] = data['referrer'][:request.url.index('?')]
+        if data['referrer']:
+            if data.get('referrer', '').startswith('https://megatransparency.com/'):
+                if '?' in data.get('referrer', ''):
+                    data['referrer'] = data['referrer'][:request.url.index('?')]
         if data['referrer'].startswith('https://accounts.google.com/AccountChooser'):
             data['referrer'] = 'https://accounts.google.com/AccountChooser'
         data['post_data'] = dict(request.form)
